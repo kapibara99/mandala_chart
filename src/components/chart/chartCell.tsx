6e807b63cf603d00071323110e935cd8b0bf9852
kapy99 <kapibara2381@gmail.com>
2025-03-02 12:48:25 +0900

    各マスのoutline styleを設定

--- src/components/chart/chartCell.tsx
+++ src/components/chart/chartCell.tsx
@@ -5,8 +5,10 @@ export default function MandalaCell({ cellType, value, isFocused, zahyou }: Mand
   const inputRef = useRef<HTMLTextAreaElement>(null);
   const [cellValue, setCellValue] = useState(value);
   const [cellFocused, setCellFocused] = useState(isFocused);
-  const handleFocus = (e: FocusEvent<HTMLElement>) => {
+  const handleFocus = (e: FocusEvent<HTMLTextAreaElement>) => {
     setCellFocused(e.currentTarget == document.activeElement);
+    const valueEnd = e.currentTarget.value.length;
+    e.currentTarget.setSelectionRange(valueEnd, valueEnd);
   };
   const handleClick = (e: MouseEvent<HTMLElement>) => {
     e.preventDefault();
@@ -14,17 +16,20 @@ export default function MandalaCell({ cellType, value, isFocused, zahyou }: Mand
       inputRef.current.focus();
     }
   };
-
+  const cellTypeStyle =
+    cellType == "title"
+      ? "outline-main-strong outline-4 -outline-offset-4 z-3" // main title cell
+      : cellType == "subTitle"
+      ? "outline-main-light outline-4 -outline-offset-4 z-2" // sub title cell
+      : "outline-base-line outline"; // item cell
   return (
-    <div
-      className="relative inline-block outline outline-base-line p-2 focus-within:ring-4 focus-within:ring-attention hover:ring-2"
-      data-zahyou={zahyou.join(",")}
-    >
+    <div className="relative p-2 break-all" data-zahyou={zahyou.join(",")}>
       {/* ツールチップ */}
       <div
         className={
-          "absolute left-0 -top-full w-64 p-4 bg-white border border-gray-200 rounded-md" +
-          " transition-opacity duration-1000 -z-10 focus-within:z-10 opacity-0 focus-within:opacity-100"
+          "absolute bottom-4/5 w-64 p-4 bg-white border border-gray-200 rounded-md " +
+          (zahyou[1] > 4 ? "right-0" : "left-0") +
+          " transition-opacity duration-1000 -z-10 focus-within:z-100 opacity-0 focus-within:opacity-100 pointer-events-none focus-within:pointer-events-auto"
         }
       >
         <textarea
@@ -33,13 +38,13 @@ export default function MandalaCell({ cellType, value, isFocused, zahyou }: Mand
           onFocus={(e) => handleFocus(e)}
           onBlur={(e) => handleFocus(e)}
           onInput={(e) => setCellValue(e.currentTarget.value)}
-          className="w-full h-20 border border-gray-300 p-2 outline-none"
+          className="w-full h-20 border border-gray-300 p-2 outline-none break-all"
         />
       </div>
 
       {/* クリック/ホバー用 */}
       <button
-        className="absolute top-0 right-0 opacity-0 w-full h-full block cursor-pointer"
+        className={`absolute top-0 right-0 w-full h-full block cursor-pointer hover:opacity-10 hover:bg-main-light ${cellTypeStyle}`}
         tabIndex={-1}
         type="button"
         onClick={(e) => handleClick(e)}
