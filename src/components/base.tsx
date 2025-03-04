0a35380c872aa11ff1e65d33f7fe28e11d901245
kapy99 <kapibara2381@gmail.com>
2025-03-04 23:02:54 +0900

    fixed tailwind variable name

--- src/components/base.tsx
+++ src/components/base.tsx
@@ -1,3 +1,3 @@
 export default function Base({ children }: { children: React.ReactNode }) {
-  return <div className="w-full min-h-screen bg-base text-base-font relative overflow-hidden font-main">{children}</div>;
+  return <div className="w-full min-h-screen bg-base text-base-font-color relative overflow-hidden font-main">{children}</div>;
 }
