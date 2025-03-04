01484f96f389d0d565863eacbdec67325c216ba2
kapy99 <kapibara2381@gmail.com>
2025-03-05 00:32:58 +0900

    add GA and Contact

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,8 +2,16 @@ import { NavLink } from "react-router";
 
 export default function Footer() {
   return (
-    <footer className="w-full text-center mt-10">
-      <NavLink to="kiyaku">利用規約</NavLink>
+    <footer className="w-full text-center mt-30 py-10">
+      <div className="flex justify-center gap-5 mb-2">
+        <a href="https://forms.gle/Bcv8mHhJvipuBhd18" className="underline hover:text-attention" target="_blank" rel="noopener">
+          お問い合わせ
+        </a>
+        <NavLink className="underline hover:text-attention" to="kiyaku">
+          利用規約
+        </NavLink>
+      </div>
+
       <p>Copyright &copy; {`${new Date().getFullYear()}`} kapy</p>
     </footer>
   );
