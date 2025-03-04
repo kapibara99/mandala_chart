9c45a611f1222f7acf4c3ba8c086708957a76dc4
kapy99 <kapibara2381@gmail.com>
2025-03-04 19:18:11 +0900

    fixed svg attr

--- src/components/downloadImage.tsx
+++ src/components/downloadImage.tsx
@@ -36,16 +36,16 @@ export default function DownloadImage() {
         className="cursor-pointer relative flex gap-1.5 px-8 py-4 bg-attention text-base-color rounded-3xl hover:opacity-60 transition font-bold shadow-md"
       >
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="24px" width="24px">
-          <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
-          <g stroke-linejoin="round" stroke-linecap="round" id="SVGRepo_tracerCarrier"></g>
+          <g strokeWidth="0" id="SVGRepo_bgCarrier"></g>
+          <g strokeLinejoin="round" strokeLinecap="round" id="SVGRepo_tracerCarrier"></g>
           <g id="SVGRepo_iconCarrier">
             {" "}
             <g id="Interface / Download">
               {" "}
               <path
-                stroke-linejoin="round"
-                stroke-linecap="round"
-                stroke-width="2"
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2"
                 stroke="#f1f1f1"
                 d="M6 21H18M12 3V17M12 17L17 12M12 17L7 12"
                 id="Vector"
