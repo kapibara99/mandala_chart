7238807a23e519bd0672908e05e6463a6096e60c
kapy99 <kapibara2381@gmail.com>
2025-03-09 10:10:59 +0900

    fixed ad component dev mode

--- src/components/admaxContents.tsx
+++ src/components/admaxContents.tsx
@@ -1,24 +1,28 @@
-"use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function AdmaxContent({ admaxID }: { admaxID: string }) {
+  const [dev] = useState(import.meta.env.DEV);
   useEffect(() => {
+    if (!dev) return;
     const script = document.createElement("script");
     script.src = "https://adm.shinobi.jp/st/t.js";
     script.async = true;
     document.body.appendChild(script);
     window.admaxads = window.admaxads || [];
     window.admaxads.push({ admax_id: admaxID, type: "switch" });
-
     return () => {
       document.body.removeChild(script);
     };
-  }, [admaxID]);
+  }, [admaxID, dev]);
 
   return (
     <>
       <div className="mt-8 flex justify-center">
-        <div className={`admax-switch inline-block`} data-admax-id={admaxID} />
+        {dev ? (
+          <img className="inline-block" src="https://placehold.jp/728x90.png" alt="AD dummy" />
+        ) : (
+          <div className={`admax-switch inline-block`} data-admax-id={admaxID} />
+        )}
       </div>
     </>
   );
